4f68c04e468ad89ba31dba8a2b3c592532556b9c
Szymon Sakowicz <giebeka@gmail.com>
2020-09-13 13:17:02 +0200

    Laravel 8 auth routes fix

--- routes/auth/auth.php
+++ routes/auth/auth.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+
 Route::get('login', fn() => view('auth.login'))->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
